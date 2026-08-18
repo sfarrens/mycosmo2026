3e40f213c7f3684188cb439feca709ad27222597
sfarrens <samuel.farrens@gmail.com>
2026-08-18 11:16:01 +0200

    Add critical density function

--- src/mycosmo/cosmology.py
+++ src/mycosmo/cosmology.py
@@ -8,3 +8,10 @@ def hubble(redshift, cosmo_dict):
     dark_energy = cosmo_dict["omega_lambda_0"]
 
     return np.sqrt(hubble_const**2 * (matter + curvature + dark_energy))
+
+def critical_density(redshift, cosmo_dict):
+    Mpc = 3.08568e22
+    G = 6.6743e-11
+    H_z_si = hubble(redshift, cosmo_dict) * 1e3 / Mpc
+
+    return (3.0 * H_z_si**2) / (8.0 * np.pi * G)
